80b8a135fefd669afb3b8167febf8cab06197f7b
Haoyang Tan <jeff.tandev@gmail.com>
2025-05-10 23:47:42 -0700

    User authentication

--- UserService/UserService.js
+++ UserService/UserService.js
@@ -1,16 +1,48 @@
 import express from "express";
-import cors from "cors";
-import axios from "axios";
+import jwt from "jsonwebtoken";
+import "./env.js";
 
-const app=express();
-const PORT=8001;
-app.use(cors());
+const app = express();
+const PORT = 8001;
+const JWT_SECRET = process.env.JWT_SECRET;
+const userInfo = { 111: "password" };
+
 app.use(express.json());
 
-app.get('/',(req, res)=>{
-    res.send("First microservice");
-})
+app.post("/register", (req, res) => {
+  const { username, password } = req.body;
+  if (!username || !password) {
+    return res
+      .status(400)
+      .json({ message: "Username or Password are required" });
+  }
+  if (userInfo[username]) {
+    return res.status(400).json({ message: "User already exist" });
+  }
+  userInfo[username] = password;
+  return res.status(200).json({ message: "User create successfully" });
+});
 
-app.listen(PORT,()=>{
-    console.log(`UserService running at http://localhost:${PORT}`);
-})
+app.post("/login", (req, res) => {
+  const { username, password } = req.body;
+  if (!username || !password) {
+    return res
+      .status(400)
+      .json({ message: "Username or Password are required" });
+  }
+  if (userInfo[username] === password) {
+    const token = jwt.sign({ username }, JWT_SECRET, { expiresIn: "1m" });
+    return res.status(200).json({ message: "Login Success", token });
+  } else {
+    return res.status(400).json({ message: "Wrong username or password" });
+  }
+});
+
+app.get("/profile", (req, res) => {
+  const username = req.headers["x-user-username"];
+  res.status(200).json({ username: `${username}` });
+});
+
+app.listen(PORT, () => {
+  console.log(`UserService running at http://localhost:${PORT}`);
+});
--- api-gateway/API_Gateway.js
+++ api-gateway/API_Gateway.js
@@ -1,10 +1,51 @@
 import express from "express";
 import cors from "cors";
+import jwt from "jsonwebtoken";
 import { createProxyMiddleware } from "http-proxy-middleware";
+import "./env.js";
 
 const app = express();
 const PORT = 8000;
+const JWT_SECRET = process.env.JWT_SECRET;
 app.use(cors());
+
+const authenticationToken = (req, res, next) => {
+  const authHeader = req.header("authorization");
+  const token = authHeader && authHeader.split(" ")[1];
+  if (!token) {
+    return res.status(400).json({ message: "No Token provided" });
+  }
+  jwt.verify(token, JWT_SECRET, (err, user) => {
+    if (err) {
+      return res.status(400).json({ message: "Token invalid" });
+    }
+    req.headers["x-user-username"] = user.username;
+    next();
+  });
+};
+
+const protectedRoutes = ["/profile", "/update-profile", "/change-password"];
+
+app.use(
+  "/UserService",
+  (req, res, next) => {
+    if (
+      protectedRoutes.some((route) => {
+        return req.path.startsWith(route);
+      })
+    ) {
+      return authenticationToken(req, res, next);
+    }
+    next();
+  },
+  createProxyMiddleware({
+    target: "http://localhost:8001",
+    changeOrigin: true,
+    pathRewrite: {
+      "^/UserService": "",
+    },
+  })
+);
 
 app.use(
   "/ContentService",
