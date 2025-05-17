30c91bb5adc620bb07e92727ab933160f689ccc1
Haoyang Tan <jeff.tandev@gmail.com>
2025-05-16 17:11:21 -0700

    express mount path change

--- ContentService/ContentService.js
+++ ContentService/ContentService.js
@@ -1,10 +1,15 @@
 import express from "express";
 import imagesRoute from "./routes/images.js";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const PORT = 8002;
 
-app.use("/", express.static("uploads"));
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 app.use("/images", imagesRoute);
 
